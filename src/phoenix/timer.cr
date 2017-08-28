08d1fb33e9e04cfb146a5000b878a09940fe5836
David Cristofaro <david@dtcristo.com>
2017-08-29 09:17:58 +1000

    Rename variables in timer

--- src/phoenix/timer.cr
+++ src/phoenix/timer.cr
@@ -5,7 +5,7 @@ module Phoenix
   # ```
   # reconnect_timer = Phoenix::Timer.new(
   #   -> { connect() },
-  #   ->(tries : UInt32) { [1000_u32, 5000_u32, 10_000_u32].at(tries - 1) { 10_000_u32 } }
+  #   ->(count : UInt32) { [1000_u32, 5000_u32, 10_000_u32].at(count - 1) { 10_000_u32 } }
   # )
   # reconnect_timer.schedule_timeout() # fires after 1000
   # reconnect_timer.schedule_timeout() # fires after 5000
@@ -13,37 +13,37 @@ module Phoenix
   # reconnect_timer.schedule_timeout() # fires after 1000
   # ```
   class Timer
-    @tries : UInt32
+    @count : UInt32
 
     # Create a basic timer with a fixed delay
-    def initialize(callback : ->, delay : UInt32)
-      initialize(callback, ->(tries : UInt32) { delay })
+    def initialize(callback : ->, delay : UInt32, repeat : Bool = false)
+      initialize(callback, ->(count : UInt32) { delay }, repeat)
     end
 
-    # Create a dynamic timer with a delay based on the number of tries
-    def initialize(@callback : ->, @delay_calc : UInt32 -> UInt32)
-      @tries = 0_u32
-      @current_id = 0_u32
+    # Create a dynamic timer with a delay based on the count
+    def initialize(@callback : ->, @delay_calc : UInt32 -> UInt32, @repeat : Bool = false)
+      @count = 0_u32
+      @last_id = 0_u32
       @active_timeouts = Hash(UInt32, Bool).new(false)
     end
 
-    # Cancels any previous `schedule_timeout` and resets the number of tries
+    # Cancels any previous `schedule_timeout` and resets the count
     def reset
-      @tries = 0_u32
-      @active_timeouts[@current_id] = false
+      @count = 0_u32
+      @active_timeouts[@last_id] = false
     end
 
     # Cancels any previous `schedule_timeout` and schedules callback
-    def schedule_timeout(repeat = false)
-      @active_timeouts[@current_id] = false
-      id = @current_id += 1
+    def schedule_timeout
+      @active_timeouts[@last_id] = false
+      id = @last_id += 1
       @active_timeouts[id] = true
       spawn do
-        sleep @delay_calc.call(@tries + 1) * 0.001
+        sleep(@delay_calc.call(@count + 1) * 0.001)
         if @active_timeouts[id]
-          @tries += 1
+          @count += 1
           @callback.call()
-          schedule_timeout(repeat) if repeat
+          schedule_timeout() if @repeat
         end
       end
       # yield immediately to start the timer ticking
