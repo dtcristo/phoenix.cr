fa0d4a6acdf0b197531140e4ab4ae31b7862c477
David Cristofaro <david@dtcristo.com>
2017-08-30 11:11:04 +1000

    Add new constructor for Message

--- src/phoenix/message.cr
+++ src/phoenix/message.cr
@@ -4,6 +4,10 @@ module Phoenix
   struct Message
     # getter :topic, :event, :payload, :ref, :join_ref
 
+    def initialize(params : NamedTuple(topic: String, event: String, payload: JSON::Any, ref: String, join_ref: String?))
+      initialize(params[:topic], params[:event], params[:payload], params[:ref], params[:join_ref])
+    end
+
     def initialize(@topic : String, @event : String, @payload : JSON::Any, @ref : String, @join_ref : String?)
     end
 
