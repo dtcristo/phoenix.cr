9db8405ae73df48b4767ea783c83d7a44cc391f0
David Cristofaro <david@dtcristo.com>
2017-09-01 00:06:26 +1000

    Implement wire protocol 2.0.0

--- src/phoenix/serializer.cr
+++ src/phoenix/serializer.cr
@@ -1,11 +1,26 @@
 module Phoenix
   module Serializer
     def self.encode(msg : Message) : String
-      msg.to_json
+      JSON.build do |json|
+        json.array do
+          json.raw msg.join_ref.to_json
+          json.raw msg.ref.to_json
+          json.string msg.topic
+          json.string msg.event
+          json.raw msg.payload.to_json
+        end
+      end
     end
 
     def self.decode(raw_payload : String) : Message
-      Message.from_json(raw_payload)
+      json = JSON.parse(raw_payload)
+      Message.new(
+        json[2].as_s,
+        json[3].as_s,
+        json[4],
+        json[1].as_s?,
+        json[0].as_s?
+      )
     end
   end
 end
